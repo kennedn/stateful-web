3270b336ab9d5af109965a83931e4bf018b02c5a
kennedn <kennedn@msn.com>
2026-01-02 19:51:23 +0000

    progress

--- hack/generate-httpasswd.sh
+++ hack/generate-httpasswd.sh
@@ -3,4 +3,4 @@ working_dir=$(dirname "$(readlink -f "$0")")
 cd "${working_dir}"
 which htpasswd &> /dev/null || sudo apt install apache2-utils
 read -rp "htpasswd username: " username
-htpasswd ../.htpasswd "${username}"
+htpasswd -c ../config/htpasswd "${username}"
