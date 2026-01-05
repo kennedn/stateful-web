9cd61ba3a619b797cd7961f393e85311a35d31e7
Kennedn <kennedn@msn.com>
2026-01-05 18:03:51 +0000

    Add configurable device cards view

--- src/components/ThermostatPanel.vue
+++ src/components/ThermostatPanel.vue
@@ -2,26 +2,40 @@
   <ThermostatView
     :loading="loading"
     :error-message="errorMessage"
-    :radiator-status="radiatorStatus"
-    :bthome-status="bthomeStatus"
-    :thermostat-status="thermostatStatus"
+    :cards="filteredCards"
+    :available-types="deviceTypes"
+    :selected-types="selectedTypes"
     @refresh="refresh"
+    @toggle-type="toggleType"
+    @select-all="selectAll"
   />
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { useThermostatStatus } from '../composables/useThermostatStatus';
+import { computed, onMounted, ref } from 'vue';
 import ThermostatView from './ThermostatView.vue';
+import { DEVICE_TYPES, useDeviceCards } from '../composables/useDeviceCards';
+import type { DeviceType } from '../composables/useDeviceCards';
 
-const {
-  loading,
-  errorMessage,
-  radiatorStatus,
-  bthomeStatus,
-  thermostatStatus,
-  refresh,
-} = useThermostatStatus();
+const { loading, errorMessage, cards, refresh } = useDeviceCards();
+const deviceTypes = DEVICE_TYPES;
+const selectedTypes = ref<DeviceType[]>([...DEVICE_TYPES]);
+
+const filteredCards = computed(() =>
+  cards.value.filter((card) => selectedTypes.value.includes(card.kind)),
+);
+
+function toggleType(type: DeviceType) {
+  if (selectedTypes.value.includes(type)) {
+    selectedTypes.value = selectedTypes.value.filter((t) => t !== type);
+  } else {
+    selectedTypes.value = [...selectedTypes.value, type];
+  }
+}
+
+function selectAll() {
+  selectedTypes.value = [...DEVICE_TYPES];
+}
 
 onMounted(() => {
   refresh();
